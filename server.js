bb0bcab8c331bc0a9c13d575314d9d78769991f9
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-10-07 16:43:05 +0530

    bug fixes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const eventRoutes = require('./routes/events');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/api/events', eventRoutes);
 app.use(errorHandler);
